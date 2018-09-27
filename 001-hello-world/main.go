8bb3148a7ba9e7b14a7b185396e76730822cb03b
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-09-27 12:21:52 +1000

    doing good

--- 001-hello-world/main.go
+++ 001-hello-world/main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	x := 42
+	fmt.Println(x)
+	y := x * x
+	fmt.Println(y)
+
 }
---
+++ 010-short-declaration-operator/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+	y := x * x
+	fmt.Println(y)
+	z := "Bond, James"
+	fmt.Println(z)
+
+}
+
+// Benjamins-MacBook-Pro:benjaminjoyce benjaminjoyce$ cd 010-short-declaration-operator/
+// Benjamins-MacBook-Pro:010-short-declaration-operator benjaminjoyce$ go run main.go
+// 42
+// 1764
+// Bond, James
